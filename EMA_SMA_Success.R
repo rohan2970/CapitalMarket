3ac13e90697299ee2f824b0909d7f3dd1b3d5173
rohan2970 <rohan2970srivastava@gmail.com>
2015-06-11 21:27:54 +0530

    Exponential & Simple Moving Average
    We have some success in Exponential Moving Average & simple Moving
    Average

---
+++ EMA_SMA_Success.R
@@ -0,0 +1,19 @@
+data1=read.csv("wipro.csv")
+dfx1 = xts(data1$Close, order.by=as.Date(data1$Date))
+mwipro=to.monthly(dfx1)
+wiproOpen=Op(mwipro)
+ts2 = ts(wiproOpen,frequency=12)
+plot(ts2,xlab="Years+1",ylab="dfx1")
+plot(decompose(ts2),xlab="years+1")
+ts1Train = window(ts2,start=1,end=5)
+ts1Test = window(ts2,start=5,end=(7-0.01))
+ts1Train
+ts1Test
+plot(ts1Train)
+lines(ma(ts1Train,order=3),col="red")
+ets1 = ets(ts1Train,model="MMM")
+ets1
+fcast1=forecast(ets1)
+plot(fcast1)
+lines(ts1Test,col="red")
+accuracy(fcast1,ts1Test)
