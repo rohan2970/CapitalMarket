53f12bb7e8fcb64da289449a0cab4b8849fed359
rohan2970 <rohan2970srivastava@gmail.com>
2015-06-17 16:34:41 +0530

    Prediction of close price of next day using EMA on last Seven days.
    This script can be used to display next day price on the web page for
    the time being.

---
+++ NextDayEMA.R
@@ -0,0 +1,22 @@
+library(quantmod)
+library(xts)
+library(caret)
+library(ggplot2)
+library(forecast)
+data1=read.csv("nestle.csv")
+data2 = data1[1:10,5]
+weighted = 10
+sumer = 0
+emasum = 0
+for(i in seq(1,10,1)){
+  emasum = emasum + data2[i]*weighted
+  sumer = sumer + weighted
+  weighted = weighted - 1
+}
+weighted
+emasum
+sumer
+
+result = emasum/sumer
+
+result
